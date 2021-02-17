a31abc9f863a845c427aa19d46430a1152f7cf0c
ikarus-balsero <osvaldoconcep@gmail.com>
2021-02-17 16:39:09 -0500

    When a user hovers over any one of the boxes, highlight all of the boxes with a different background color.

---
+++ main.js
@@ -0,0 +1,11 @@
+function highlightBoxes() {
+    document.getElementById("boxOne").style.backgroundColor = 'blue';
+    document.getElementById("boxTwo").style.backgroundColor = 'red';
+    document.getElementById("boxThree").style.backgroundColor = 'black';
+}
+
+function resetBoxes() {
+    document.getElementById("boxOne").style.backgroundColor = 'yellow';
+    document.getElementById("boxTwo").style.backgroundColor = 'lightseagreen';
+    document.getElementById("boxThree").style.backgroundColor = 'greenyellow';
+}
