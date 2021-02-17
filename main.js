81899faf42a151dab5c5c89b469fa47b9c746c55
ikarus-balsero <osvaldoconcep@gmail.com>
2021-02-17 16:51:54 -0500

    Appending messages based on box selection by the user.

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ function resetBoxes() {
     document.getElementById("boxTwo").style.backgroundColor = 'lightseagreen';
     document.getElementById("boxThree").style.backgroundColor = 'greenyellow';
 }
+
+function appendMessage(message) {
+    // append message only if there is none
+    if (!document.getElementById("resultMessage")) {
+        const p = document.createElement("p");
+        p.setAttribute("id", "resultMessage");
+        const node = document.createTextNode(message);
+        p.appendChild(node);
+        const element = document.getElementById("resultBox");
+        element.appendChild(p);
+    }
+}
